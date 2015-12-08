46b77eee741f5b0e95b9e36ed10848deb7f13f0a
Henrik Ottehall <henot294@gmail.com>
2015-12-08 12:43:34 +0100

    Add clickSettings and getCardIndex functions. Minor changes to variables.

--- GreenMemory/MainWindow.xaml.cs
+++ GreenMemory/MainWindow.xaml.cs
@@ -25,14 +25,18 @@ namespace GreenMemory
     {
         private MemoryModel gameModel;
         private int pickedCard = -1;
-        private int rows = 4;
-        private int columns = 4;
+        private int numRows = 4;
+        private int numColumns = 4;
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
             newGame();
             ((settings.Content as StackPanel).Children[0] as Button).Click += clickNewGame;
+            ((settings.Content as StackPanel).Children[1] as Button).Click += clickSettings;
         }
 
         /// <summary>
@@ -40,7 +44,7 @@ namespace GreenMemory
         /// </summary>
         private void newGame()
         {
-            gameModel = new MemoryModel(rows * columns);
+            this.gameModel = new MemoryModel(this.numRows * this.numColumns);
 
             Brush[] br = new Brush[8]{Brushes.LightBlue, Brushes.Blue, Brushes.Yellow, 
                                         Brushes.Green, Brushes.Red, Brushes.Orange, Brushes.Aqua, Brushes.Maroon};
@@ -51,39 +55,70 @@ namespace GreenMemory
             cardGrid.Children.Clear();
             cardGrid.RowDefinitions.Clear();
             cardGrid.ColumnDefinitions.Clear();
-            pickedCard = -1;
+            this.pickedCard = -1;
 
             // Set number of rows
-            for (int i = 0; i < rows; ++i)
+            for (int i = 0; i < this.numRows; ++i)
             {
                 cardGrid.RowDefinitions.Add(new RowDefinition());
             }
 
             // Set number of columns
-            for (int i = 0; i < columns; ++i)
+            for (int i = 0; i < this.numColumns; ++i)
             {
                 cardGrid.ColumnDefinitions.Add(new ColumnDefinition());
             }
 
             CardView.BackgroundImage = Brushes.Black;
-            int[] deck = gameModel.GetDeck();
+            int[] deck = this.gameModel.GetDeck();
 
+            // Set up the cards
             for (int ix = 0; ix < deck.Length; ++ix)
             {
                 CardView card = new CardView(deck[ix], br[deck[ix]]);
                 card.Margin = new Thickness(5);
-                Grid.SetColumn(card, (ix % columns));
-                Grid.SetRow(card, (ix / rows));
+                Grid.SetColumn(card, (ix % this.numColumns));
+                Grid.SetRow(card, (ix / this.numRows));
                 card.MouseUp += clickCard;
                 cardGrid.Children.Add(card);
             }
         }
         
+        /// <summary>
+        /// Handler for new game button click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         void clickNewGame(object sender, RoutedEventArgs e)
         {
             newGame();
         }
 
+        /// <summary>
+        /// Handler for settings button click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void clickSettings(object sender, RoutedEventArgs e)
+        {
+            // TODO: Settingswindow
+        }
+
+        /// <summary>
+        /// Returns the grid index for the given card
+        /// </summary>
+        /// <param name="card"></param>
+        /// <returns></returns>
+        private int getCardIndex(CardView card)
+        {
+            return (Grid.GetRow(card) * this.numColumns) + Grid.GetColumn(card);
+        }
+
+        /// <summary>
+        /// Handler for card click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         void clickCard(object sender, MouseButtonEventArgs e)
         {
             CardView card = sender as CardView;
@@ -92,15 +127,13 @@ namespace GreenMemory
             {
                 card.FlipCard();
 
-                if (pickedCard != -1)
+                if (this.pickedCard != -1)
                 {
-                    int row = Grid.GetRow(card);
-                    int column = Grid.GetColumn(card);
-                    int? correct = gameModel.PickTwoCards(pickedCard, (row * columns) + column, 1);
+                    int? correct = this.gameModel.PickTwoCards(this.pickedCard, getCardIndex(card), 1);
 
                     if (correct != null)
                     {
-                        if (gameModel.IsGameOver())
+                        if (this.gameModel.IsGameOver())
                         {
                             // TODO: Show gameover.
                         }
@@ -111,7 +144,7 @@ namespace GreenMemory
                     }
                     else
                     {
-                        CardView secondCard = this.CardGrid.Children[pickedCard] as CardView;
+                        CardView secondCard = this.CardGrid.Children[this.pickedCard] as CardView;
                         CardGrid.IsEnabled = false;
 
                         Task.Delay(400).ContinueWith(_ =>
@@ -125,13 +158,11 @@ namespace GreenMemory
                         });
                     }
 
-                    pickedCard = -1;
+                    this.pickedCard = -1;
                 }
                 else
                 {
-                    int row = Grid.GetRow(card);
-                    int column = Grid.GetColumn(card);
-                    pickedCard = (row * columns) + column;
+                    this.pickedCard = getCardIndex(card);
                 }
             }
         }
