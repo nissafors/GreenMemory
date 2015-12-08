67dc6606cca1ec6c9d01ac916bb2e9fcb315862e
Henrik Ottehall <henot294@gmail.com>
2015-12-08 10:52:49 +0100

    CardGrid view is Grid. Can play, sort of.

--- GreenMemory/MainWindow.xaml.cs
+++ GreenMemory/MainWindow.xaml.cs
@@ -22,25 +22,39 @@ namespace GreenMemory
     public partial class MainWindow : Window
     {
         private MemoryModel gameModel;
+        private int pickedCard = -1;
+        private int rows = 4;
+        private int columns = 4;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            gameModel = new MemoryModel(16);
+            gameModel = new MemoryModel(rows * columns);
 
-            //TODO: Fill CardGrid with CardView
             Brush[] br = new Brush[8]{Brushes.LightBlue, Brushes.Blue, Brushes.Yellow, 
                                         Brushes.Green, Brushes.Red, Brushes.Orange, Brushes.Aqua, Brushes.Maroon};
-            UniformGrid cardGrid = this.CardGrid as UniformGrid;
-            CardView.BackgroundImage = Brushes.Black;
-            cardGrid.Rows = 6;
-            cardGrid.Columns = 6;
+            Grid cardGrid = this.CardGrid as Grid;
+            int[] deck = gameModel.GetDeck();
 
-            for(int ix = 0; ix < 36; ++ix)
+            for (int i = 0; i < rows; ++i)
             {
-                CardView card = new CardView(ix % 16, br[ix % 8]);
+                cardGrid.RowDefinitions.Add(new RowDefinition());
+            }
+            
+            for (int i = 0; i < columns; ++i)
+            {
+                cardGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+            CardView.BackgroundImage = Brushes.Black;
+
+            for(int ix = 0; ix < deck.Length; ++ix)
+            {
+                CardView card = new CardView(deck[ix], br[deck[ix]]);
                 card.Margin = new Thickness(5);
+                Grid.SetColumn(card, (ix % columns));
+                Grid.SetRow(card, (ix / rows));
                 card.MouseUp += clickCard;
                 cardGrid.Children.Add(card);
             }
@@ -49,7 +63,46 @@ namespace GreenMemory
         void clickCard(object sender, MouseButtonEventArgs e)
         {
             CardView card = sender as CardView;
-            card.FlipCard();
+
+            if (!card.IsUp())
+            {
+                card.FlipCard();
+
+                if (pickedCard != -1)
+                {
+                    int row = Grid.GetRow(card);
+                    int column = Grid.GetColumn(card);
+                    int? correct = gameModel.PickTwoCards(pickedCard, (row * columns) + column, 1);
+
+                    if (correct != null)
+                    {
+                        if (gameModel.IsGameOver())
+                        {
+                            // TODO: Show gameover.
+                        }
+                        else
+                        {
+                            //player1.pairs.Content = gameModel.GetScore(1);
+                        }
+                    }
+                    else
+                    {
+                        // TODO: Add delay
+                        card.FlipCard();
+                        CardView secondCard = this.CardGrid.Children[pickedCard] as CardView;
+                        secondCard.FlipCard();
+                    }
+
+                    pickedCard = -1;
+                }
+                else
+                {
+                    int row = Grid.GetRow(card);
+                    int column = Grid.GetColumn(card);
+                    pickedCard = (row * columns) + column;
+
+                }
+            }
         }
     }
 }
